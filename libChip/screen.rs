7c47c0a55adeaddc272c0f6e0af978684eb5bb2d
Andrew Goacher <andrewgoacher@outlook.com>
2019-06-26 14:21:43 +0100

    Some kind of screen implementation working

--- libChip/screen.rs
+++ libChip/screen.rs
@@ -1,62 +1,83 @@
-use console::Term;
 use console::Style;
+use console::Term;
 
-pub struct Screen {
+pub trait Screen {
+    fn draw(&mut self);
+    fn on(&mut self);
+    fn off(&mut self);
+    fn set_pixel(&mut self, x: i32, y: i32, on: bool);
+}
+
+pub struct ConsoleScreen {
     display: Vec<bool>,
     width: i32,
     height: i32,
-    first_run: bool
+    first_run: bool,
 }
 
-impl Screen {
-    pub fn new(width: i32, height: i32) -> Screen {
-        Screen {
+impl ConsoleScreen {
+    pub fn new(width: i32, height: i32) -> ConsoleScreen {
+        ConsoleScreen {
             display: vec![false; (width * height) as usize],
             width: width,
             height: height,
-            first_run: true
+            first_run: true,
         }
     }
+}
 
-    pub fn draw(&mut self) {
-        let term = Term::stdout();
+impl Screen for ConsoleScreen {
+    fn draw(&mut self) {
+        let term = Term::buffered_stdout();
         if !self.first_run {
-            term.clear_last_lines(self.height as usize).expect("Should have worked");
+            term.move_cursor_up(self.height as usize)
+                .expect("Some error here");
+            // term.clear_screen().expect("clear screen");
+            // term.flush().expect("flushy");
         }
         self.first_run = false;
 
         let on_style = Style::new().bg(console::Color::Blue);
         let off_style = Style::new().bg(console::Color::Yellow);
+        let mut strings: Vec<String> = Vec::new();
 
-        for w in 0 .. self.width {
-            for h in 0 .. self.height {
+        for w in 0..self.width {
+            for h in 0..self.height {
                 let idx = ((self.width * w) + h) as usize;
                 let flag = self.display[idx];
                 if flag {
-                    print!("{}", on_style.apply_to("  "));
+                    strings.push(format!("{}", on_style.apply_to("  ")));
                 } else {
-                    print!("{}", off_style.apply_to("  "));
+                    strings.push(format!("{}", off_style.apply_to("  ")));
                 }
             }
-            println!();
+            term.write_line(&strings.join(""))
+                .expect("Error expectedly errored");
+            strings.clear();
+            term.flush().expect("FLUHSY");
         }
     }
 
-    pub fn on(&mut self) {
-          for w in 0 .. self.width {
-            for h in 0 .. self.height {
+    fn on(&mut self) {
+        for w in 0..self.width {
+            for h in 0..self.height {
                 let idx = ((self.width * w) + h) as usize;
                 self.display[idx] = true;
             }
         }
     }
 
-        pub fn off(&mut self) {
-          for w in 0 .. self.width {
-            for h in 0 .. self.height {
+    fn off(&mut self) {
+        for w in 0..self.width {
+            for h in 0..self.height {
                 let idx = ((self.width * w) + h) as usize;
                 self.display[idx] = false;
             }
         }
+    }
+
+    fn set_pixel(&mut self, x: i32, y: i32, on: bool) {
+        let idx = ((self.width * y) + x) as usize;
+        self.display[idx] = on;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
 extern crate lib_chip;
-use lib_chip::screen::Screen;
+use lib_chip::screen::{ConsoleScreen, Screen};
 
 use std::thread;
 use std::time::Duration;
 
 fn main() {
     println!("Hello, world!");
-    let mut screen = Screen::new(20, 20);
+    let mut screen = ConsoleScreen::new(20, 20);
     let mut on = true;
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
 
     loop {
-        if on {
-            screen.off();
-            on = false;
-        } else {
-            screen.on();
-            on = true;
+        for _ in 0..5 {
+            if x == 20 {
+                x = 0;
+                y += 1;
+            }
+
+            if y == 20 {
+                y = 0;
+                x = 0;
+                on = !on;
+            }
+            screen.set_pixel(x, y, on);
+            x += 1;
         }
+        thread::sleep(Duration::from_millis(10));
         screen.draw();
-        thread::sleep(Duration::from_millis(2000));
     }
 }
