66b89e0f245df44fe01580bdbf564388d6e18ef7
Andrew Goacher <andrewgoacher@outlook.com>
2019-07-14 19:03:15 +0100

    Added sdl code into project

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,77 @@ use lib_chip::{
     rom::Rom
 };
 
-fn main() {
-    const WIDTH:i32 = 64i32;
-    const HEIGHT:i32 = 32i32;
+// fn main() {
+//     const WIDTH:i32 = 64i32;
+//     const HEIGHT:i32 = 32i32;
 
-    let screen = ScreenParams::new()
-        .with_height(HEIGHT)
-        .with_width(WIDTH)
-        .for_console()
-        .build();
+//     let screen = ScreenParams::new()
+//         .with_height(HEIGHT)
+//         .with_width(WIDTH)
+//         .for_console()
+//         .build();
     
-    let file = r"C:\Users\andre\Downloads\Cave.ch8";
-    let rom = Rom::load(file).expect("Rom loaded");
-    let mut chip8 = Chip8::new(Box::new(screen));
+//     let file = r"C:\Users\andre\Downloads\Cave.ch8";
+//     let rom = Rom::load(file).expect("Rom loaded");
+//     let mut chip8 = Chip8::new(Box::new(screen));
 
-    chip8.load_rom(rom);
-   // chip8.run();
+//     chip8.load_rom(rom);
+//    // chip8.run();
+// }
+
+
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::rect::Rect;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
+pub fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+    let texture_creator = canvas.texture_creator();
+
+    let mut texture = texture_creator.create_texture_streaming(PixelFormatEnum::RGB24, 256, 256)
+        .map_err(|e| e.to_string())?;
+    // Create a red-green gradient
+    texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+        for y in 0..256 {
+            for x in 0..256 {
+                let offset = y*pitch + x*3;
+                buffer[offset] = x as u8;
+                buffer[offset + 1] = y as u8;
+                buffer[offset + 2] = 0;
+            }
+        }
+    })?;
+
+    canvas.clear();
+    canvas.copy(&texture, None, Some(Rect::new(100, 100, 256, 256)))?;
+    canvas.copy_ex(&texture, None,
+        Some(Rect::new(450, 100, 256, 256)), 30.0, None, false, false)?;
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump()?;
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..}
+                | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+    }
+
+    Ok(())
 }
