0ba15e57d56934cf42589d7b725b93f54c9c2496
Andrew Goacher <andrewgoacher@outlook.com>
2019-08-02 13:54:36 +0100

    Add files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 extern crate lib_chip;
+use lib_chip::{
+    state::State,
+    rom::Rom,
+    memory::Memory
+};
 //use lib_chip::sdl_display::*;
 // use lib_chip::{
 //     // screen::{builder::{ScreenParams}},
@@ -16,19 +21,26 @@ pub fn main() -> Result<(), String> {
     const width: u32 = 640;
     const height: u32 = 320;
 
-     let sdl_context = sdl2::init()?;
-        let video_subsystem = sdl_context.video()?;
+    let mut state = State::new();
+    let rom = Rom::load("./tetris.ch8").expect("Failed to load rom");
+    let mut memory = Memory::new();
+    memory.set_range(0x200, rom.read_all());
 
-        let window = video_subsystem.window("Chip8 Emulator", width, height)
-            .position_centered()
-            .opengl()
-            .build()
-            .map_err(|e| e.to_string())?;
+    state = state.run();
 
-        let mut canvas = window.into_canvas()
-            .accelerated()
-            .present_vsync()
-            .build().expect("Failed to build canvas");
+    //  let sdl_context = sdl2::init()?;
+    //     let video_subsystem = sdl_context.video()?;
+
+    //     let window = video_subsystem.window("Chip8 Emulator", width, height)
+    //         .position_centered()
+    //         .opengl()
+    //         .build()
+    //         .map_err(|e| e.to_string())?;
+
+    //     let mut canvas = window.into_canvas()
+    //         .accelerated()
+    //         .present_vsync()
+    //         .build().expect("Failed to build canvas");
 
         // let mut display = SdlDisplay::new(canvas, width, height);
         // let black: Vec<u8> = vec![0xF, width & height];
@@ -44,25 +56,25 @@ pub fn main() -> Result<(), String> {
 
       
 
-        let mut event_pump = sdl_context.event_pump()?;
+        // let mut event_pump = sdl_context.event_pump()?;
 
         // let mut keys: Vec<u8> = Vec::new();
-        'running: loop {
-            for event in event_pump.poll_iter() {
-                match event {
-                    Event::Quit {..}
-                    | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                        break 'running
-                    },
-                    _ => {}
-                }
-            }
+        // 'running: loop {
+        //     for event in event_pump.poll_iter() {
+        //         match event {
+        //             Event::Quit {..}
+        //             | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+        //                 break 'running
+        //             },
+        //             _ => {}
+        //         }
+        //     }
 
             // display.draw();
 
             // The rest of the game loop goes here...
             // emulator.frame(self, keys);
-        }
+        // }
         
         Ok(())
 }
