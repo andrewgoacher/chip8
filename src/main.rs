da2bf876c42af92fd4b16df21bb3360071726eca
Andrew Goacher <andrewgoacher@outlook.com>
2019-08-04 22:33:23 +0100

    Some kind of rendering

--- src/main.rs
+++ src/main.rs
@@ -14,27 +14,34 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::render::Texture;
 
-fn draw(texture: &mut Texture, screen: &Vec<u8>, width: u32, height: u32) -> Result<(), String> {
+fn draw(texture: &mut Texture, screen: &Vec<u8>, width: u32, height: u32, scale: u32) -> Result<(), String> {
     texture.with_lock(None, |buffer: &mut [u8], _pitch: usize| {
         for y in 0 .. height {
             for x in 0 .. width {
                 let idx = ((width * y) + x) as usize;
-                let offset = idx; 
-
                 let slot = screen[idx];
                 let color = if slot == 0 { 0xFF } else { 0x00 };
-
-                buffer[offset*3] = color;
-                buffer[(offset*3)+1] = color;
-                buffer[(offset*3)+2] = color;
+                
+                for i in 0..scale {
+                    for j in 0..scale {
+                        let ix = (x*scale) + i;
+                        let iy = (y*scale) + j;
+                        let tote_width = width * scale;
+                        let tote_height = height * scale;
+                        let offset = ((tote_width * iy) + ix) as usize;
+            
+                        buffer[offset*3] = color;
+                        buffer[(offset*3)+1] = color;
+                        buffer[(offset*3)+2] = color;
+                    }
+                }
             }
         }
     })
 }
 
 pub fn main() -> Result<(), String> {
-    // const WIDTH_SCALE: u32 = 10;
-    // const HEIGHT_SCALE: u32 = 10;
+    const SCALE: u32 = 10;
 
     const EMU_WIDTH: u32 = 64;
     const EMU_HEIGHT: u32 = 32;
@@ -49,7 +56,7 @@ pub fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
-    let window = video_subsystem.window("rust-sdl2 demo: Video", EMU_WIDTH, EMU_HEIGHT)
+    let window = video_subsystem.window("rust-sdl2 demo: Video", EMU_WIDTH * SCALE, EMU_HEIGHT * SCALE)
         .position_centered()
         .opengl()
         .build()
@@ -59,7 +66,7 @@ pub fn main() -> Result<(), String> {
     let texture_creator = canvas.texture_creator();
 
     let mut texture = texture_creator.create_texture_streaming(PixelFormatEnum::RGB24,
-     EMU_WIDTH, EMU_HEIGHT)
+     EMU_WIDTH*SCALE, EMU_HEIGHT*SCALE)
         .map_err(|e| e.to_string())?;
 
     let mut event_pump = sdl_context.event_pump()?;
@@ -85,8 +92,8 @@ pub fn main() -> Result<(), String> {
             canvas.clear();
         }
         if state.draw_flag {
-            draw(&mut texture, &screen, EMU_WIDTH, EMU_HEIGHT)?;
-            canvas.copy(&texture, None, Some(Rect::new(0, 0, EMU_WIDTH, EMU_HEIGHT)))?;
+            draw(&mut texture, &screen, EMU_WIDTH, EMU_HEIGHT, SCALE)?;
+            canvas.copy(&texture, None, Some(Rect::new(0, 0, EMU_WIDTH * SCALE, EMU_HEIGHT * SCALE)))?;
         }
         if state.clear_flag || state.draw_flag {
             state.draw_flag = false;
